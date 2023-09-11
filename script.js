22a4cd215c6fe25a505e057c08c4b5d305e15c09
Samuel <alakindeakintomiwa@gmail.com>
2023-09-11 02:16:32 +0100

    web page

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-document.getElementById("slackUserName").textContent = "Slack Name: Alakinde akintomiwa";
-document.getElementById("slackDisplayImage").src = "HNG-Internship/tom.jpg";
+document.getElementById("slackUserName").textContent =
+  "Slack Name: Alakinde akintomiwa";
+document.getElementById("slackDisplayImage").src = "tom.jpg";
 document.getElementById("slackDisplayImage").alt = "Alakinde akintomiwa";
 document.getElementById("myTrack").textContent = "Track: Frontend";
 document.getElementById("githubURL").href =
   "https://github.com/Shamoche/HNG-Internship";
 
-  function updateTimeAndDay() {
-    const date = new Date();
-    const options = { timeZone: 'Europe/Paris', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-    document.getElementById('currentUTCTime').textContent = "Current Time (GMT+1): " + date.toLocaleTimeString('en-GB', options);
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    document.getElementById('currentDayOfTheWeek').textContent = "Current Day of the Week: " + days[date.getDay()];
+function updateTimeAndDay() {
+  const date = new Date();
+  const options = {
+    timeZone: "Europe/Paris",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  document.getElementById("currentUTCTime").textContent =
+    "Current Time (GMT+1): " + date.toLocaleTimeString("en-GB", options);
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  document.getElementById("currentDayOfTheWeek").textContent =
+    "Current Day of the Week: " + days[date.getDay()];
 }
 
 setInterval(updateTimeAndDay, 1000);
